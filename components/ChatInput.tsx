aaca075f764c07cbfdaae041246767ecc2d3ec07
Akaikenlol <keithken972@gmail.com>
2024-08-30 21:51:41 +0630

    feat: Implementing ChatInput

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -4,8 +4,14 @@ import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import React from "react";
 import { Send } from "lucide-react";
+import { ChatInputProps } from "@/types";
 
-const ChatInput = () => {
+const ChatInput = ({
+	handleInputChange,
+	handleSubmit,
+	input,
+	setInput,
+}: ChatInputProps) => {
 	return (
 		<div className="z-10 bg-zinc-900 absolute bottom-0 left-0 w-full">
 			<div className="mx-2 flex flex-row gap-3 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
@@ -15,6 +21,15 @@ const ChatInput = () => {
 							<Textarea
 								rows={4}
 								autoFocus
+								onChange={handleInputChange}
+								value={input}
+								onKeyDown={(e) => {
+									if (e.key === "Enter" && !e.shiftKey) {
+										e.preventDefault();
+										handleSubmit();
+										setInput("");
+									}
+								}}
 								placeholder="Enter your question..."
 								className="resize-none bg-zinc-800 hover:bg-zinc-900 rounded-xl text-base"
 							/>
